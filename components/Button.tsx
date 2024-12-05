f568ea1b59e223c14c9837ea7bef1e0e80fb23bc
HANSOL <hansol302@gmail.com>
2024-12-05 21:00:06 +0900

    Feat: Button 컴포넌트 props로 버튼 눌렀을 때에 대한 함수 추가

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,11 +4,12 @@ type ButtonProps = {
   style: ViewStyle;
   textStyle: TextStyle;
   buttonText: string;
+  onPressFunc?: () => void;
 };
 
-export default function Button({ buttonText, style, textStyle }: ButtonProps) {
+export default function Button({ buttonText, style, textStyle, onPressFunc }: ButtonProps) {
   return (
-    <Pressable style={style}>
+    <Pressable style={style} onPressIn={onPressFunc}>
       <Text style={textStyle}>{buttonText}</Text>
     </Pressable>
   );
