06908856aeeb61ba6ed1e50dbcd4f8d0316a66c6
HANSOL <hansol302@gmail.com>
2024-12-08 15:57:44 +0900

    Style: 메모 둘러보기에 적용된 Custom Header 삭제

--- app/ar.tsx
+++ app/ar.tsx
@@ -4,7 +4,7 @@ import PlusSvg from "../assets/images/plus.svg";
 import { MAIN_PAGE, MEMO_LIST_PAGE } from "@/constants/Pages";
 import { ViroARScene, ViroARSceneNavigator } from "@reactvision/react-viro";
 import { router } from "expo-router";
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function ar() {
   function arScene() {
@@ -23,8 +23,6 @@ export default function ar() {
 
   return (
     <>
-      <SafeAreaView style={styles.headerContainer} />
-
       <ViroARSceneNavigator
         autofocus={true}
         initialScene={{ scene: arScene }}
@@ -34,14 +32,14 @@ export default function ar() {
 
       <View style={styles.bottomContainer}>
         <Pressable style={styles.iconContainer} onPress={handleMoveToHome}>
-          <HomeSvg width="30%" height="30%" color="#343a40" />
+          <HomeSvg width="30%" height="30%" color="#343A40" />
           <Text style={styles.iconText}>{MAIN_PAGE}</Text>
         </Pressable>
         <Pressable style={styles.plusIconContainer} onPress={handleMoveToARGrid}>
           <PlusSvg width="65%" height="65%" color="#6CA0DC" />
         </Pressable>
         <View style={styles.iconContainer}>
-          <MemoListSvg width="30%" height="30%" color="#343a40" />
+          <MemoListSvg width="30%" height="30%" color="#343A40" />
           <Text style={styles.iconText}>{MEMO_LIST_PAGE}</Text>
         </View>
       </View>
@@ -50,9 +48,6 @@ export default function ar() {
 }
 
 const styles = StyleSheet.create({
-  headerContainer: {
-    flex: 0.1,
-  },
   arContainer: {
     flex: 1,
   },
