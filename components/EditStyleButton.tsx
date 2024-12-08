320f948a4910dd478e7dee6ebcac8e6e64718a39
HANSOL <hansol302@gmail.com>
2024-12-09 02:03:11 +0900

    Feat: 메모 스타일 변경 버튼 컴포넌트 구현

---
+++ components/EditStyleButton.tsx
@@ -0,0 +1,41 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+type EditStyleButtonProps = {
+  text: string;
+  imageName: number;
+};
+
+export default function EditStyleButton({ text, imageName }: EditStyleButtonProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{text}</Text>
+      <View style={styles.imageContainer}>
+        <Image source={imageName} style={styles.image} resizeMode="contain" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 55,
+    height: 55,
+    borderRadius: 55 / 2,
+    backgroundColor: "#D7D7D7",
+  },
+  text: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  image: {
+    width: 35,
+    height: 35,
+  },
+});
