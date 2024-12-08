7784fd42a9c9841da22c3c57e8901987fc1ddc3c
HANSOL <hansol302@gmail.com>
2024-12-08 15:59:18 +0900

    Refactor: AR 그리드 화면 내 Custom Header 삭제 및 하단 네비게이션 내 등록 버튼 삭제

--- app/arGrid.tsx
+++ app/arGrid.tsx
@@ -1,10 +1,9 @@
 import HomeSvg from "../assets/images/home.svg";
 import MemoListSvg from "../assets/images/memoList.svg";
-import PlusSvg from "../assets/images/plus.svg";
 import { MAIN_PAGE, MEMO_LIST_PAGE } from "@/constants/Pages";
 import { ViroARScene, ViroARSceneNavigator, ViroQuad } from "@reactvision/react-viro";
 import { router } from "expo-router";
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function arGrid() {
   function arGridScene() {
@@ -22,8 +21,6 @@ export default function arGrid() {
 
   return (
     <>
-      <SafeAreaView style={styles.headerContainer} />
-
       <ViroARSceneNavigator
         autofocus={true}
         initialScene={{ scene: arGridScene }}
@@ -33,25 +30,20 @@ export default function arGrid() {
 
       <View style={styles.bottomContainer}>
         <Pressable style={styles.iconContainer} onPress={handleMoveToHome}>
-          <HomeSvg width="30%" height="30%" color="#343a40" />
+          <HomeSvg width="30%" height="30%" color="#343A40" />
           <Text style={styles.iconText}>{MAIN_PAGE}</Text>
         </Pressable>
-        <Pressable style={styles.plusIconContainer}>
-          <PlusSvg width="65%" height="65%" color="#6CA0DC" />
-        </Pressable>
-        <View style={styles.iconContainer}>
-          <MemoListSvg width="30%" height="30%" color="#343a40" />
+        <View style={styles.blankContainer}></View>
+        <Pressable style={styles.iconContainer}>
+          <MemoListSvg width="30%" height="30%" color="#343A40" />
           <Text style={styles.iconText}>{MEMO_LIST_PAGE}</Text>
-        </View>
+        </Pressable>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  headerContainer: {
-    flex: 0.1,
-  },
   arContainer: {
     flex: 1,
   },
@@ -69,9 +61,7 @@ const styles = StyleSheet.create({
     marginTop: 8,
     color: "#343A40",
   },
-  plusIconContainer: {
+  blankContainer: {
     flex: 1,
-    alignItems: "center",
-    marginTop: 3,
   },
 });
