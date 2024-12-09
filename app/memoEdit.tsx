7ba2e829c7e4787c82d7e767d29d5ddeda3061ce
HANSOL <hansol302@gmail.com>
2024-12-09 10:50:50 +0900

    Design: 메모 등록 페이지 내 메모 UI 구현

--- app/memoEdit.tsx
+++ app/memoEdit.tsx
@@ -1,11 +1,15 @@
 import EditStyleButton from "@/components/EditStyleButton";
 import { COLOR, RESIZE, SHAPE, TEXT } from "@/constants/EditStyle";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function memoEdit() {
   return (
     <View style={styles.container}>
-      <View style={styles.editViewContainer}></View>
+      <View style={styles.editViewContainer}>
+        <View style={styles.memo}>
+          <TextInput />
+        </View>
+      </View>
       <View style={styles.editTabContainer}>
         <EditStyleButton text={TEXT} imageName={require("../assets/images/text.png")} />
         <EditStyleButton text={SHAPE} imageName={require("../assets/images/shape.png")} />
@@ -23,6 +27,14 @@ const styles = StyleSheet.create({
   editViewContainer: {
     flex: 8,
     backgroundColor: "#E5E8E8",
+  },
+  memo: {
+    width: 300,
+    height: 300,
+    backgroundColor: "#F9E79F",
+    borderColor: "#000000",
+    borderWidth: 0.5,
+    margin: "auto",
   },
   editTabContainer: {
     flex: 2,
