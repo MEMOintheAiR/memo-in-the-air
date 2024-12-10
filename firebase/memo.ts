09733097cd146674dd13ececab07b39de32775e4
HANSOL <hansol302@gmail.com>
2024-12-10 10:07:02 +0900

    Refactor: 메모 저장 시 메모 저장 구조 변경

--- firebase/memo.ts
+++ firebase/memo.ts
@@ -1,9 +1,9 @@
 import { database } from "@/firebaseConfig";
 import { Database, ref, set } from "firebase/database";
 
-export async function createMemo(memoId: string, userId: string, content: string) {
-  await set(ref(database as Database, `memo/${memoId}`), {
-    userId,
+export async function createMemo(userId: string, memoId: string, content: string) {
+  console.log(memoId, userId, content);
+  await set(ref(database as Database, `memo/${userId}/${memoId}`), {
     content,
     createdAt: new Date(),
     updatedAt: new Date(),
