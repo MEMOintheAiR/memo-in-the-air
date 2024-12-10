a1c13c865cd8af85b3adc2891b34be49450bfa3e
HANSOL <hansol302@gmail.com>
2024-12-10 22:48:45 +0900

    Feat: 메모 좌표 파악하는 함수 구현 및 좌표 파악하는 동안 표시되는  모달 구현

--- app/arGrid.tsx
+++ app/arGrid.tsx
@@ -1,17 +1,48 @@
 import HomeSvg from "@/assets/images/home.svg";
 import MemoListSvg from "@/assets/images/memoList.svg";
 import { MAIN_PAGE, MEMO_LIST_PAGE } from "@/constants/Pages";
-import { ViroARScene, ViroARSceneNavigator, ViroQuad } from "@reactvision/react-viro";
+import { useBoundStore } from "@/store/useBoundStore";
+import { ViroARScene, ViroARSceneNavigator, ViroFlexView } from "@reactvision/react-viro";
+import * as Location from "expo-location";
 import { router } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function arGrid() {
+export default function ArGrid() {
+  const setMemoLocation = useBoundStore((state) => state.setMemoLocation);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
   function arGridScene() {
     return (
-      <ViroARScene>
-        <ViroQuad position={[0, 0, -5]} height={1.5} width={1.5} opacity={0.5} />
-      </ViroARScene>
+      <>
+        <ViroARScene>
+          <ViroFlexView
+            position={[0, 0, -5]}
+            scale={[1.7, 1.7, 0]}
+            onClickState={handleClickGrid}
+            style={{ backgroundColor: "#FFFF00", opacity: 0.5 }}
+          />
+        </ViroARScene>
+      </>
     );
+  }
+
+  async function handleClickGrid() {
+    setIsModalVisible(true);
+
+    const { coords } = await Location.getCurrentPositionAsync({ accuracy: 6 });
+
+    if (coords) {
+      setMemoLocation({
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        altitude: coords.altitude,
+      });
+      setIsModalVisible(false);
+
+      router.dismiss();
+      router.push("/memoEdit");
+    }
   }
 
   function handleMoveToHome() {
@@ -21,6 +52,15 @@ export default function arGrid() {
 
   return (
     <>
+      <Modal visible={isModalVisible} transparent={true} animationType="none">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalText}>
+            위치 파악 중 . . . 🧭 {"\n"}
+            잠시만 기다려주세요 ⏳
+          </Text>
+        </View>
+      </Modal>
+
       <ViroARSceneNavigator
         autofocus={true}
         initialScene={{ scene: arGridScene }}
@@ -63,5 +103,27 @@ const styles = StyleSheet.create({
   },
   blankContainer: {
     flex: 1,
+  },
+  modalContainer: {
+    width: "80%",
+    height: "15%",
+    margin: "auto",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    shadowColor: "#343A40",
+  },
+  modalText: {
+    fontSize: 25,
+    color: "#343A40",
+    textAlign: "center",
+    lineHeight: 35,
   },
 });
