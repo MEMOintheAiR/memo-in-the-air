e9d7336cc258dfcefc34a3e73ee35975ebb7f485
HANSOL <hansol302@gmail.com>
2024-12-11 15:18:19 +0900

    Feat: 앱 진입 시, 사용자의 위치 설정 여부 파악하는 로직 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,27 @@
 import Home from "./home";
 import { useBoundStore } from "@/store/useBoundStore";
 import { checkUserId } from "@/utils/uuid";
+import * as Location from "expo-location";
+import { router } from "expo-router";
 import { useEffect } from "react";
 
 export default function Index() {
   const setUserId = useBoundStore((state) => state.setUserId);
 
+  async function checkLocationPermission() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        router.replace("/locationPermission");
+      }
+    } catch {
+      router.replace("/locationPermission");
+    }
+  }
+
   useEffect(() => {
+    checkLocationPermission();
+
     const userId: Promise<string> = checkUserId();
     setUserId(userId);
   }, []);
