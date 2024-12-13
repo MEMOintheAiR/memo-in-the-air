f1b0ec2464b44380d75c09e37c3b6d29726d39ca
HANSOL <hansol302@gmail.com>
2024-12-13 11:41:47 +0900

    Feat: + 버튼 클릭하면 웹뷰에 메모 그리드 띄우는 코드 전달하는 로직 구현

--- app/arWebview.tsx
+++ app/arWebview.tsx
@@ -3,22 +3,45 @@ import MemoListSvg from "@/assets/images/memoList.svg";
 import PlusSvg from "@/assets/images/plus.svg";
 import { MAIN_PAGE, MEMO_LIST_PAGE } from "@/constants/Pages";
 import { router } from "expo-router";
+import { useRef, useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
 
-export default function webView() {
+export default function ARWebView() {
+  const webViewRef = useRef<WebView>(null);
+  const [isGridVisible, setIsGridVisible] = useState<boolean>(false);
+
   function handleMoveToHome() {
     router.back();
+  }
+
+  function handleClickPlusButton() {
+    if (!isGridVisible) {
+      setIsGridVisible(true);
+      webViewRef.current?.injectJavaScript(`
+        const aPlane = document.createElement("a-plane");
+        aPlane.setAttribute("position", "0 1.7 -5");
+        aPlane.setAttribute("rotation", "0 0 0");
+        aPlane.setAttribute("width", "1.5");
+        aPlane.setAttribute("height", "1.5");
+        aPlane.setAttribute("material", "color: #FFFF4C; opacity: 0.7;");
+        aPlane.setAttribute("id", "memoGrid");
+
+        document.getElementById("aScene")?.appendChild(aPlane);
+        true;
+      `);
+    }
   }
 
   return (
     <>
       <WebView
-        style={styles.arContainer}
+        ref={webViewRef}
+        source={{ uri: "https://memointheair-ar.netlify.app/" }}
         javaScriptEnabled={true}
         mediaPlaybackRequiresUserAction={false}
         allowsInlineMediaPlayback={true}
-        source={{ uri: "https://memointheair-ar.netlify.app/" }}
+        style={styles.arContainer}
       />
 
       <View style={styles.bottomContainer}>
@@ -26,7 +49,7 @@ export default function webView() {
           <HomeSvg width="30%" height="30%" color="#343A40" />
           <Text style={styles.iconText}>{MAIN_PAGE}</Text>
         </Pressable>
-        <Pressable style={styles.plusIconContainer}>
+        <Pressable style={styles.plusIconContainer} onPress={handleClickPlusButton}>
           <PlusSvg width="65%" height="65%" color="#6CA0DC" />
         </Pressable>
         <View style={styles.iconContainer}>
