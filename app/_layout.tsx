72a1a502d3a7ca611277e3394870abe50a3662a6
HANSOL <hansol302@gmail.com>
2024-12-15 10:24:16 +0900

    Chore: Viro 라이브러리 삭제 및 그에 따른 로직 삭제

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { CREATE_MEMO_PAGE } from "@/constants/Pages";
 import { Stack } from "expo-router";
 import "react-native-reanimated";
 
@@ -8,28 +7,6 @@ export default function RootLayout() {
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="home" options={{ headerShown: false }} />
       <Stack.Screen name="loading" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="ar"
-        options={{
-          title: "",
-          headerStyle: { backgroundColor: "#FFFFFF" },
-          headerBackVisible: false,
-        }}
-      />
-      <Stack.Screen
-        name="arGrid"
-        options={{
-          title: CREATE_MEMO_PAGE,
-          headerStyle: {
-            backgroundColor: "#FFFFFF",
-          },
-          headerTintColor: "#343A40",
-          headerTitleStyle: {
-            fontSize: 20,
-          },
-          headerBackVisible: false,
-        }}
-      />
       <Stack.Screen name="memoEdit" options={{ headerShown: false }} />
       <Stack.Screen
         name="arWebview"
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,6 @@
-import { router } from "expo-router";
 import { ActivityIndicator, SafeAreaView, StyleSheet } from "react-native";
 
 export default function Loading() {
-  window.setTimeout(() => {
-    router.dismiss();
-    router.push("/arGrid");
-  }, 1000);
-
   return (
     <SafeAreaView style={styles.container}>
       <ActivityIndicator size="large" />
