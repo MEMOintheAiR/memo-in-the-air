a98143defd11be301d272833ed6133ef63f939bb
HANSOL <hansol302@gmail.com>
2024-12-16 13:57:32 +0900

    Feat: 사용자의 메모 목록에 대한 전역 상태 추가

--- store/memoSlice.ts
+++ store/memoSlice.ts
@@ -6,12 +6,31 @@ type locationType = {
   altitude: number | null;
 };
 
+type memoType = {
+  [key: string]: memoIntoType;
+};
+
+type memoIntoType = {
+  latitude: string;
+  longitude: string;
+  altitude: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
 export interface MemoSlice {
+  memoList: memoType[];
+  setMemoList: (memoList: memoType[] | Promise<memoType[]>) => void;
   memoLocation: locationType;
   setMemoLocation: ({ latitude, longitude, altitude }: locationType) => void;
 }
 
 export const createMemoSlice: StateCreator<MemoSlice> = (set) => ({
+  memoList: [],
+  setMemoList: async (memoList: memoType[] | Promise<memoType[]>) => {
+    set({ memoList: await memoList });
+  },
   memoLocation: {
     latitude: 0,
     longitude: 0,
