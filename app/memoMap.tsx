2b2ad840904eadf197443d894467441c0542940b
HANSOL <hansol302@gmail.com>
2025-01-03 23:19:38 +0900

    Fix: 메모 목록 조회하는 로직 삭제

--- app/memoMap.tsx
+++ app/memoMap.tsx
@@ -1,25 +1,12 @@
-import { getMemoList } from "@/firebase/memo";
 import { useBoundStore } from "@/store/useBoundStore";
 import { fixToSixDemicalPoints } from "@/utils/number";
-import { useEffect } from "react";
 import { SafeAreaView } from "react-native";
 import MapView from "react-native-map-clustering";
 import { Marker } from "react-native-maps";
 
 export default function MemoMap() {
-  const userId = useBoundStore((state) => state.userId);
   const userLocation = useBoundStore((state) => state.userLocation);
   const memoList = useBoundStore((state) => state.memoList);
-  const setMemoList = useBoundStore((state) => state.setMemoList);
-
-  async function getUserMemoList(): Promise<void> {
-    const memoList = await getMemoList(userId);
-    setMemoList(memoList || []);
-  }
-
-  useEffect(() => {
-    getUserMemoList();
-  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
