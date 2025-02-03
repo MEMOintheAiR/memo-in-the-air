f0f0edb995ef962e52c5b98fa227348698abc5f0
HANSOL <hansol302@gmail.com>
2025-02-04 02:54:55 +0900

    Fix: 폰트 로드하는 로직 위치 변경 및 폰트 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,7 @@
-import * as Font from "expo-font";
 import { Stack } from "expo-router";
-import { useEffect, useState } from "react";
-import { setCustomText } from "react-native-global-props";
 import "react-native-reanimated";
 
 export default function RootLayout() {
-  const [isFontLoaded, setIsFontLoaded] = useState(false);
-
-  useEffect(() => {
-    async function getFont() {
-      await Font.loadAsync({
-        "SUITE-Light": require("@/assets/fonts/SUITE-Light.otf"),
-        "SUITE-Regular": require("@/assets/fonts/SUITE-Regular.otf"),
-        "SUITE-Medium": require("@/assets/fonts/SUITE-Medium.otf"),
-        "SUITE-SemiBold": require("@/assets/fonts/SUITE-SemiBold.otf"),
-        "SUITE-Bold": require("@/assets/fonts/SUITE-Bold.otf"),
-      });
-
-      setIsFontLoaded(true);
-      setCustomText({
-        style: {
-          fontFamily: "SUITE-Regular, SUITE-Medium, SUITE-SemiBold, SUITE-Bold",
-        },
-      });
-    }
-    getFont();
-  }, []);
-
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import Home from "./home";
 import { useBoundStore } from "@/store/useBoundStore";
 import { checkUserId } from "@/utils/uuid";
+import * as Font from "expo-font";
 import * as Location from "expo-location";
 import { router } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { setCustomText } from "react-native-global-props";
 
 export default function Index() {
   const setUserId = useBoundStore((state) => state.setUserId);
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
 
   async function checkLocationPermission() {
     try {
@@ -19,12 +22,35 @@ export default function Index() {
     }
   }
 
+  async function getFont() {
+    await Font.loadAsync({
+      "SUITE-Light": require("@/assets/fonts/SUITE-Light.otf"),
+      "SUITE-Regular": require("@/assets/fonts/SUITE-Regular.otf"),
+      "SUITE-Medium": require("@/assets/fonts/SUITE-Medium.otf"),
+      "SUITE-SemiBold": require("@/assets/fonts/SUITE-SemiBold.otf"),
+      "SUITE-Bold": require("@/assets/fonts/SUITE-Bold.otf"),
+      "Roboto-Medium": require("@/assets/fonts/Roboto-Medium.ttf"),
+    });
+
+    setIsFontLoaded(true);
+    setCustomText({
+      style: {
+        fontFamily: "SUITE-Regular, SUITE-Medium, SUITE-SemiBold, SUITE-Bold, Roboto-Medium",
+      },
+    });
+  }
+
   useEffect(() => {
     checkLocationPermission();
+    getFont();
 
     const userId: Promise<string> = checkUserId();
     setUserId(userId);
   }, []);
+
+  if (!isFontLoaded) {
+    return null;
+  }
 
   return <Home />;
 }
