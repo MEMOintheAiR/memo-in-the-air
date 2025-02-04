fc6d65ee50c13be90c847f2ec8c776f3d51cc744
HANSOL <hansol302@gmail.com>
2025-02-05 03:34:52 +0900

    Feat: 하단 탭에 아이콘 적용

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,26 @@
+import MapMarker from "@/assets/images/mapMarker.svg";
+import Memo from "@/assets/images/memo.svg";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#5E8BCE", headerShown: false }}>
       <Tabs.Screen
         name="index"
         options={{
           title: "메모 목록",
+          tabBarIcon: ({ focused }) => (
+            <MapMarker width={25} height={25} color={focused ? "#5E8BCE" : "#808b96"} />
+          ),
         }}
       />
       <Tabs.Screen
         name="memoCreate"
         options={{
           title: "메모 등록",
+          tabBarIcon: ({ focused }) => (
+            <Memo width={30} height={30} color={focused ? "#5E8BCE" : "#808b96"} />
+          ),
         }}
       />
       <Tabs.Screen
